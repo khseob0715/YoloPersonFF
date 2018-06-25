c1fc36a0a0a28e926935a4a0332d162bbab5e5b4
Han Seob Kim <khseob0715@gmail.com>
2018-06-25 14:37:11 +0900

    layout design

--- app/src/main/java/com/aiden/yolopersonff/MainActivity.java
+++ app/src/main/java/com/aiden/yolopersonff/MainActivity.java
@@ -1,13 +1,28 @@
 package com.aiden.yolopersonff;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.aiden.yolopersonff.Fragment.Movie_Fragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+// fragment를 불러오는 소스코드.
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.youtube, new Movie_Fragment());
+        fragmentTransaction.commit();
+
+
     }
 }
