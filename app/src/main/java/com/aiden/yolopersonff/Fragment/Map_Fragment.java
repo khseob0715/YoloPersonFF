c6f8a2a21580eefc16fe1313f3bf4fb245d67f0d
Han Seob Kim <khseob0715@gmail.com>
2018-06-25 16:53:53 +0900

    layout init

---
+++ app/src/main/java/com/aiden/yolopersonff/Fragment/Map_Fragment.java
@@ -0,0 +1,31 @@
+package com.aiden.yolopersonff.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.aiden.yolopersonff.R;
+
+/**
+ * Created by Aiden on 2018-06-25.
+ */
+
+public class Map_Fragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_map,null);
+
+        return view;
+    }
+}
