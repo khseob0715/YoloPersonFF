499fcbfa2ba4163d9624187d58fe91e13e77faa1
LAPTOP-QKNGTG8B\hongbin <fusis1@naver.com>
2018-06-25 20:02:31 +0900

    ADD GPS SERVICE

--- app/src/main/java/com/aiden/yolopersonff/Fragment/Map_Fragment.java
+++ app/src/main/java/com/aiden/yolopersonff/Fragment/Map_Fragment.java
@@ -1,12 +1,19 @@
 package com.aiden.yolopersonff.Fragment;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.aiden.yolopersonff.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -16,6 +23,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import static android.location.LocationManager.GPS_PROVIDER;
+
 /**
  * Created by Aiden on 2018-06-25.
  */
@@ -23,11 +32,11 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class Map_Fragment extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Double lat, lon;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
 
     }
 
@@ -48,9 +57,83 @@ public class Map_Fragment extends Fragment implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        StartLocationService();
+    }
+
+    public static void ShowMyLocaion(Double lat, Double lon, GoogleMap googleMap) {
+        try {
+            LatLng nowLocation = new LatLng(lat, lon);
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.position(nowLocation);
+            markerOptions.title("now location");
+
+            googleMap.clear();
+
+            googleMap.addMarker(markerOptions);
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(nowLocation));
+            googleMap.animateCamera(CameraUpdateFactory.zoomTo(17));
+
+        }   catch (IllegalStateException e)   {
+        }
+    }
+
+    private class GPSListener implements android.location.LocationListener {
+        @Override
+        public void onLocationChanged(Location location) {
+            lat = location.getLatitude();
+            lon = location.getLongitude();
+            ShowMyLocaion(lat,lon,mMap);
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+            lat = null;
+            lon = null;
+        }
+    }
+
+    private void StartLocationService() {
+        LocationManager manager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        GPSListener gpsListener = new GPSListener();
+        long minTime = 1000;
+        float minDistance = 0;
+        try {   //GPS 위치 요청
+            if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+
+                return;
+            }
+
+            try {
+                Location location = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if (location == null) {
+                    location = manager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                }
+                ShowMyLocaion(location.getLatitude(), location.getLongitude(), mMap);
+            } catch (NullPointerException e) {
+            }
+
+            manager.requestLocationUpdates(GPS_PROVIDER, minTime, minDistance, (android.location.LocationListener) gpsListener);
+
+            // location request with network
+            manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, minTime, minDistance, (android.location.LocationListener) gpsListener);
+
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
     }
 }
